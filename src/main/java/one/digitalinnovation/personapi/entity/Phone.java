fd2e1bb394bc1c66a9c1360bd7b6a6e4a8294e78
rogs <pilique11@gmail.com>
2021-08-20 00:48:37 -0300

    entities ok

---
+++ src/main/java/one/digitalinnovation/personapi/entity/Phone.java
@@ -0,0 +1,32 @@
+package one.digitalinnovation.personapi.entity;
+
+import lombok.*;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Phone {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+//    @Enumerated(EnumType.STRING)
+//    @Column(nullable = false)
+//    private PhoneType type;
+
+    @Column(nullable = false)
+    private String number;
+
+
+}
